1241ae94a2db2078b5a8ab80968e4eb937687798
DevvSakib <devvsakib@gmail.com>
2022-10-28 22:18:00 +0600

    extra padding

--- src/component/Roadmap/Card.js
+++ src/component/Roadmap/Card.js
@@ -4,7 +4,7 @@ function Card() {
     return (
         <>
             <hr></hr>
-            <div className='container-style flex flex-col sm:flex-row px-5 sm:px-0  py-6 mt-10 justify-between mb-10'>
+            <div className='container-style flex flex-col sm:flex-row px-5 sm:px-20  py-6 mt-10 justify-between mb-10'>
                 {
                     arr.map(items => (
                         <div className="rounded-md backdrop-blur-[30px] z-0 overflow-visible drop-shadow-3xl sm:drop-shadow-4xl p-10 my-3">
